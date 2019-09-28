2fb232f0b1828d204e4c30698c8a24f2fcafdc2a
Lukas Würzburger <lukas.w@me.com>
2019-09-28 12:05:12 +0200

    Create info page for Stack Overflow friends

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function openMail() {
+	var et1 = "l" + "u" + "k" + "a" + "s";
+	var et2 = ".";
+	var et3 = "w";
+	var et4 = "@";
+	var et5 = "m" + "e";
+	var et6 = ".";
+	var et7 = "c" + "o" + "m";
+	var href = "mailto:" + et1 + et2 + et3 + et4 + et5 + et6 + et7 + '?subject=Homepage%20Imprint';
+	window.location.href = href;
+}
